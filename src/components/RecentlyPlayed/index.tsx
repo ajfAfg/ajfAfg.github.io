540768897de119161d252fdec6aac00dd1a098d7
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2025-08-12 00:04:42 +0900

    #36: Last.fm の Recently Palyed を表示するコンポーネントを追加

---
+++ src/components/RecentlyPlayed/index.tsx
@@ -0,0 +1,8 @@
+export const RecentlyPlayed = () => (
+	<a href="https://www.last.fm/user/ajfAfg" target="_blank" rel="noopener">
+		<img
+			src="https://lastfm-recently-played.vercel.app/api?user=ajfAfg"
+			alt="Last.fm Recently Played"
+		/>
+	</a>
+);
